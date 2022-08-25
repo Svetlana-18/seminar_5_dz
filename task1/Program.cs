672d90a348a4fa649f60bb6344afda7162d84b35
Svetlana-18 <vasilevichsvetlana@yandex.ru>
2022-08-25 03:10:15 +0300

    создала папки, gitignore

---
+++ task1/Program.cs
@@ -0,0 +1,55 @@
+﻿// // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива:  ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+
+if (lenght == 0 || lenght < 0)
+{
+    Console.Write("Вы ввели недопустимое значение");
+    return;
+}
+
+int[] RandArray(int len)
+{
+    int[] arrays = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < arrays.Length; i++)
+    {
+        arrays[i] = rnd.Next(0, 100);
+
+    }
+    return arrays;
+}
+
+
+void PrintArray(int[] res1)
+{
+    Console.Write("[");
+    for (int i = 0; i < res1.Length - 1; i++)
+    {
+        Console.Write($"{res1[i]}, ");
+
+    }
+    Console.Write($"{res1[res1.Length - 1]}]");
+}
+
+int SumElem(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        sum += array[i];
+        i = i + 1;
+    }
+    return sum;
+}
+int[] res = RandArray(lenght);
+PrintArray(res);
+
+
+int sumelem = SumElem(res);
+Console.WriteLine($"   Сумма элементов, стоящих на нечётных позициях (индексах) равна: {sumelem}");
---
+++ task2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементами массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+
+
+double[] CreateArrayRndInt(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {Math.Round(array[i], 2)} ");
+    }
+    Console.Write($"]");
+}
+
+double Ren(double[] arr)
+{
+    double d = 0;
+    double min = arr[0];
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+
+    {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+        d = max - min;
+    }
+    d = max - min;
+    return d;
+}
+double[] arr = CreateArrayRndInt(8, 1, 11);
+PrintArray(arr);
+Console.WriteLine();
+
+double ren = Ren(arr);
+Console.WriteLine($" {Math.Round(ren, 2)} ");
+
+
+
